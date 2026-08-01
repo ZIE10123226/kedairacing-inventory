e2e0d9fc5432c912c74a4c9cf605eab64a0a20e4
ZIE10123226 <senzie.10123226@mahasiswa.unikom.ac.id>
2026-08-02 01:40:51 +0700

    Catch and display Vercel 500 error

--- api/index.php
+++ api/index.php
@@ -3,26 +3,31 @@
  * Forward Vercel requests to normal Laravel routing
  */
 
-require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+try {
+    require __DIR__ . '/../vendor/autoload.php';
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Ensure /tmp/storage exists for Vercel
-$storagePath = $_ENV['APP_STORAGE'] ?? '/tmp/storage';
-$app->useStoragePath($storagePath);
+    // Ensure /tmp/storage exists for Vercel
+    $storagePath = $_ENV['APP_STORAGE'] ?? '/tmp/storage';
+    $app->useStoragePath($storagePath);
 
-$dirs = [
-    $storagePath . '/app/public',
-    $storagePath . '/framework/cache/data',
-    $storagePath . '/framework/sessions',
-    $storagePath . '/framework/testing',
-    $storagePath . '/framework/views',
-    $storagePath . '/logs',
-];
+    $dirs = [
+        $storagePath . '/app/public',
+        $storagePath . '/framework/cache/data',
+        $storagePath . '/framework/sessions',
+        $storagePath . '/framework/testing',
+        $storagePath . '/framework/views',
+        $storagePath . '/logs',
+    ];
 
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0777, true);
+    foreach ($dirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
     }
-}
 
-$app->handleRequest(Illuminate\Http\Request::capture());
+    $app->handleRequest(Illuminate\Http\Request::capture());
+} catch (\Throwable $e) {
+    http_response_code(500);
+    echo "<h1>Real Error Found:</h1><pre>" . $e->getMessage() . "\n" . $e->getTraceAsString() . "</pre>";
+}
